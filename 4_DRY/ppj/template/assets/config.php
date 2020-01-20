4a4224deb57d96a17d9262eb4d0e6d0bf9d633ce
yoann <yoann.valero7@gmail.com>
2020-01-20 22:40:06 +0100

    DRY avancé

---
+++ 4_DRY/ppj/template/assets/config.php
@@ -0,0 +1,5 @@
+<?php 
+define("cheminTemplate", "/4_DRY/ppj/template"); 
+define("cheminImages", "/images"); 
+define("cheminCss", "./assets/css/style.css"); 
+?>
---
+++ 4_DRY/ppj/template/menu.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <?php include_once 'assets/config.php'; ?>
+    <head>
+        <!-- Theme Made By www.w3schools.com - No Copyright -->
+        <title>Bootstrap Theme Simply Me</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link rel="stylesheet" href="<?php $cheminCss ?>">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+    </head>
+    
+    <body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a id="logo" class="navbar-brand" href="index.html"><img src="images/bird.jpg"  alt="logo" width="50px;"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="index.html">ACCUEIL</a>
+                    </li>
+                    <li>
+                        <a href="produits.html">NOS PRODUITS</a>
+                    </li>
+                    <li>
+                        <a href="blog.html">BLOG</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">CONTACT</a>
+                    </li>
+                    <li>
+                        <a href="about.html">ABOUT</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
