63d7094422d43871f46128d2223f7591191f9748
yoann <yoann.valero7@gmail.com>
2020-01-21 16:46:28 +0100

    avancement

--- 4_DRY/ppj/template/config/config.php
+++ 4_DRY/ppj/template/config/config.php
@@ -1,9 +1,17 @@
 <?php 
+// title
 define("title", "Pierre, Paul, Jacquot");
-define("cheminTemplate", "/4_DRY/ppj/template"); 
+
+// nav, footer
+define("cheminMenus", "template-parts/menu.php");
+define("cheminFooter", "template-parts/footer.php");
+ 
+//Images, CSS
 define("cheminImages", "assets/img"); 
 define("cheminCss", "./assets/css/style.css"); 
+
 define("cheminFonctions", " 4_DRY/fonctions/fonctions.php");
 
+// Rooter
 define('root', ['accueil' => 'accueil.php', 'contact' => 'contact.php', 'apropos' => 'apropos.php', 'blog' => 'blog.php', 'nosproduits' => 'nosproduits.php'])
 ?>
--- 4_DRY/ppj/template/index.php
+++ 4_DRY/ppj/template/index.php
@@ -1,33 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include 'config/config.php'; ?>
-
-<head>
-    <!-- Theme Made By www.w3schools.com - No Copyright -->
-    <title><?= title ?></title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <link rel="stylesheet" href="<?= cheminCss ?>">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-</head>
-
-<body>
-    <!-- NAV -->
-    <?php include 'template-parts/menu.php';
-    include 'config/router.php';
+    
+    <?php include 'config/config.php'; ?>
+    <head>
+        <!-- Theme Made By www.w3schools.com - No Copyright -->
+        <title><?= title ?></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link rel="stylesheet" href="<?= cheminCss ?>">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+    </head>
+    
+    <body>
+        <!-- NAV -->
+        <?php
+            include cheminMenus;
+            include 'config/router.php';
   
-    $init = new Router;
-    $init->getTemplate(root, 'accueil.php');
+        $init = new Router;
+        $init->getTemplate(root, 'accueil.php');
 
-    ?>
-    <!-- Footer -->
-    <?php require 'template-parts/footer.php' ?>
+        ?>
+        <!-- Footer -->
+        <?php require cheminFooter; ?>
 
-</body>
+    </body>
 
 </html>
